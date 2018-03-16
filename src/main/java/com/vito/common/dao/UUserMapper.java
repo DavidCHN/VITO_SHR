4c9f4af4bc2116332751d717ee244e9ffb8b8f0a
Sino <Sino@DESKTOP-C5PKF2P.mshome.net>
2018-03-16 11:18:00 +0800

    update code

---
+++ src/main/java/com/vito/common/dao/UUserMapper.java
@@ -0,0 +1,28 @@
+package com.vito.common.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.vito.common.model.UUser;
+import com.vito.permission.bo.URoleBo;
+
+public interface UUserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UUser record);
+
+    int insertSelective(UUser record);
+
+    UUser selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UUser record);
+
+    int updateByPrimaryKey(UUser record);
+
+	UUser login(Map<String, Object> map);
+
+	UUser findUserByEmail(String email);
+
+	List<URoleBo> selectRoleByUserId(Long id);
+
+}
